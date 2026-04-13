c0e6e27e2a4808ef48bae4232c09b0d3644e58d1
Fred Howland <fred@derfwins.com>
2026-04-13 01:28:04 +0000

    fix: NATS, Guacamole DB init, nginx proxy for remote desktop, backend deps

--- docker/postgres/init/01_extensions.sql
+++ docker/postgres/init/01_extensions.sql
@@ -1,3 +1,12 @@
--- Create database extensions
+-- Guacamole database initialization
+-- Run as part of PostgreSQL init
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
+
+-- Create guacamole database if not exists
+-- (Guacamole will handle schema creation on first boot)
+SELECT 'CREATE DATABASE guacamole_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'guacamole_db')\gexec
+
+-- Grant access to guacamole user
+GRANT ALL PRIVILEGES ON DATABASE guacamole_db TO openrmm;
